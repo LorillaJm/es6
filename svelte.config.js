da69e1e7190b7ba52b295810cfa01470fcd866b4
EdDev <133908849+EdDev2001@users.noreply.github.com>
2025-12-01 14:36:43 +0800

    Feat: update credentials

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
-
-export default config;
+export default {
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html' // SPA fallback
+    })
+  }
+};
